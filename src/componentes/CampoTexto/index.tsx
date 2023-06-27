60a6bb01ebfa0e16f4457aefd49a8321248abca1
Rafael Santos Corrêa <101223032+Faelsc@users.noreply.github.com>
2023-06-27 13:52:03 -0300

    :construction: migrando js para tsx

---
+++ src/componentes/CampoTexto/index.tsx
@@ -0,0 +1,34 @@
+import './CampoTexto.css'
+
+interface campoTextoProps {
+    aoAlterado: (valor: string) => void
+    placeholder: string
+    label: string
+    valor: string
+    obrigatorio: boolean
+}
+
+const CampoTexto = ({ aoAlterado, label, obrigatorio, placeholder, valor }: campoTextoProps) => {
+
+    const placeholderModificada = `${placeholder}...` 
+
+    const aoDigitado = (evento: React.ChangeEvent<HTMLInputElement>) => {
+        aoAlterado(evento.target.value)
+    }
+
+    return (
+        <div className="campo-texto">
+            <label>
+                {label}
+            </label>
+            <input 
+                value={valor} 
+                onChange={aoDigitado} 
+                required={obrigatorio} 
+                placeholder={placeholderModificada}             
+            />
+        </div>
+    )
+}
+
+export default CampoTexto
